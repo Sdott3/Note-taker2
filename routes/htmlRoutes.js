e4f5a1768b5ba01dbe3b6b74e14c35c52b9c096e
sdott3 <sdotthomas347@gmail.com>
2022-09-24 13:39:09 -0400

    update htmlroutes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,7 +4,12 @@ const router = require('express').Router();
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
-
+router.post('/notes', (req, res) => {
+    saveData
+        .addNote(req.body)
+        .then((note) => res.json(note))
+        .catch(err => res.status(500).json(err));
+});
 
 
 
