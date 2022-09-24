03bc764932ee846c54e2a22f6d6894eda0110454
sdott3 <sdotthomas347@gmail.com>
2022-09-24 12:54:55 -0400

    server.js complete

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-const express = require("express"):
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// Use apiRoutes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
